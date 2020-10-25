25e9ca8a89127c2739d88f2b5af53ba80a79527f
Your Name <you@example.com>
2020-10-25 19:21:30 -0300

    praticando orientacão a objeto com JavaScript

---
+++ Conta/ContaCorrente.js
@@ -0,0 +1,19 @@
+import {Conta} from "./Conta.js";
+
+export class contaCorrente extends Conta{
+    static numeroDeContas = 0;
+    constructor(cliente, agencia){
+        super(0, cliente, agencia);
+        contaCorrente.numeroDeContas += 1;
+    }
+
+    teste(){
+        super.teste();
+        console.log("teste na classe conta corrente");
+    }
+    
+    sacar(valor){
+            let taxa = 1.1;
+            return this._sacar(valor,taxa);
+    }
+}
